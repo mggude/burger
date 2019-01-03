383c420de589b05c094a8a9194d738dfcf65cf6a
Grace Gude <grace@Graces-MacBook-Pro.local>
2019-01-03 15:45:33 -0700

    updating burger app

---
+++ db/seeds.sql
@@ -0,0 +1,6 @@
+INSERT INTO burgers (name) VALUES ('Avocado');
+INSERT INTO burgers (name) VALUES ('Bacon');
+INSERT INTO burgers (name, devoured) VALUES ('Breakfast', true);
+INSERT INTO burgers (name, devoured) VALUES ('Pizza', true);
+INSERT INTO burgers (name, devoured) VALUES ('Barbeque', true);
+INSERT INTO burgers (name) VALUES ('Cheese');
